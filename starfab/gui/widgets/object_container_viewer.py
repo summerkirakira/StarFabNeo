50166b4bcdeb2fcdbaf6ed5b9488afcf0d2c5c00
Ventorvar <ventorvar@gmail.com>
2022-04-10 22:22:13 -0400

    New ObjectContainerInstance classes that are used in creating the hierarchy of object containers to retain contextual information for each object container instance.

--- starfab/gui/widgets/object_container_viewer.py
+++ starfab/gui/widgets/object_container_viewer.py
@@ -1,21 +1,14 @@
-import json
-import logging
-from functools import partial
 from pathlib import Path
 
 from qtpy import uic
 
-from scdatatools.forge import dftypes
-from scdatatools.forge.utils import geometry_for_record
-
+from scdatatools.sc.object_container import ObjectContainerInstance, ObjectContainer
 from starfab import get_starfab
-from starfab.gui import qtw, qtc, qtg
-from starfab.resources import RES_PATH
-from starfab.models.common import ContentItem
-from starfab.gui.widgets.editor import Editor
+from starfab.gui import qtw
 from starfab.gui.widgets.common import CollapsableWidget
-from starfab.plugins import plugin_manager
 from starfab.hooks import COLLAPSABLE_OBJECT_CONTAINER_WIDGET
+from starfab.plugins import plugin_manager
+from starfab.resources import RES_PATH
 
 SUPPORTED_OBJECT_CONTAINER_FILE_FORMATS = [
     ".socpak",
@@ -43,41 +36,36 @@ def widget_for_attr(name, value):
 
 
 class LazyObjectContainerWidget(CollapsableWidget):
-    def __init__(self, child_id: str, attrs: dict, *args, **kwargs):
-        self.attrs = attrs.copy()
-        self.attrs['child_id'] = child_id
-        self.name = self.attrs['name']
-        self.label = self.attrs.get('label', self.attrs.get('entityName', self.name))
-        super().__init__(self.label, expand=False, layout=qtw.QVBoxLayout, *args, **kwargs)
+    def __init__(self, child: ObjectContainerInstance, *args, **kwargs):
+        self.child = child
+        super().__init__(child.label, expand=False, layout=qtw.QVBoxLayout, *args, **kwargs)
         self._loaded = False
 
     def expand(self):
         if not self._loaded:
-            self.obj_widget = ObjectContainerView(self.name, extra_attrs=self.attrs, no_scroll=True, parent=self)
+            self.obj_widget = ObjectContainerView(self.child, no_scroll=True, parent=self)
             self.content.layout().addWidget(self.obj_widget)
             self._loaded = True
         super().expand()
 
 
 class LazyContainerChildrenWidget(CollapsableWidget):
-    def __init__(self, object_container, additional_children=None, *args, **kwargs):
+    def __init__(self, object_container, *args, **kwargs):
         super().__init__(f"Children", expand=False, layout=qtw.QVBoxLayout, *args, **kwargs)
-        self.additional_children = additional_children or {}
         self.object_container = object_container
         self._loaded = False
 
     def expand(self):
         if not self._loaded:
-            children = {**self.object_container.children, **self.additional_children}
-            for child_id, attrs in sorted(children.items(), key=lambda v: v[1].get('label', v[0])):
-                child_widget = LazyObjectContainerWidget(child_id=child_id, attrs=attrs)
+            for child_label, child in sorted(self.object_container.children.items(), key=lambda v: v[0]):
+                child_widget = LazyObjectContainerWidget(child=child)
                 self.content.layout().addWidget(child_widget)
             self._loaded = True
         super().expand()
 
 
 class ObjectContainerView(qtw.QWidget):
-    def __init__(self, info_or_path, extra_attrs=None, no_scroll=False, *args, **kwargs):
+    def __init__(self, info_or_path_or_container, extra_attrs=None, no_scroll=False, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
         uic.loadUi(str(RES_PATH / "ui" / "ChunkedObjView.ui"), self)
@@ -96,17 +84,25 @@ class ObjectContainerView(qtw.QWidget):
         # TODO: support dynamically adding actions for object containers from plugins
         self.obj_actions_frame.setVisible(False)
 
-        if isinstance(info_or_path, (str, Path)):
-            self.info = None
-            self.path = Path(info_or_path)
-            self.name = Path(info_or_path)
+        self.info = None
+        self.object_container = None
+        self.name = None
+
+        if isinstance(info_or_path_or_container, (str, Path)):
+            self.path = Path(info_or_path_or_container)
+            self.name = Path(info_or_path_or_container)
+        elif isinstance(info_or_path_or_container, (ObjectContainer, ObjectContainerInstance)):
+            self.object_container = info_or_path_or_container
+            self.path = Path(self.object_container.socpak.filename)
+            self.name = self.object_container.name
         else:
-            self.info = info_or_path
+            self.info = info_or_path_or_container
             self.path = self.info.path
             self.name = self.info.name
 
         try:
-            self.object_container = self.starfab.sc.oc_manager.load_socpak(self.path.as_posix())
+            if self.object_container is None:
+                self.object_container = self.starfab.sc.oc_manager.load_socpak(self.path.as_posix())
         except KeyError:
             self.object_container = None
 
@@ -120,7 +116,8 @@ class ObjectContainerView(qtw.QWidget):
             self.obj_info.addRow("Error", l)
             return
 
-        l = qtw.QLineEdit(self.object_container.attrs.get('name', self.name))
+        attrs = self.object_container.as_dict().copy()
+        l = qtw.QLineEdit(attrs.get('name', self.name))
         l.setReadOnly(True)
         self.obj_info.addRow("Name", l)
 
@@ -128,9 +125,7 @@ class ObjectContainerView(qtw.QWidget):
         l.setReadOnly(True)
         self.obj_info.addRow("Path", l)
 
-        attrs = self.object_container.attrs.copy()
         attrs.update(self.extra_attrs)
-        additional_children = attrs.pop('children') if 'children' in attrs else []
         attrs_to_show = sorted(attrs.keys(), key=str.casefold)
         attrs_to_show.remove('name')  # name comes first ^
 
@@ -138,7 +133,6 @@ class ObjectContainerView(qtw.QWidget):
             self.obj_info.addRow(attr, widget_for_attr(attr, attrs[attr]))
 
         self.children_widget = LazyContainerChildrenWidget(self.object_container,
-                                                           additional_children=additional_children,
                                                            parent=self.obj_content_widget)
         self.obj_content.insertWidget(self.obj_content.count() - 1, self.children_widget)
 
